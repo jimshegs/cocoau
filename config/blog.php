8ace9c719543270d704fe0f6dd8d9ce3cc63f5b1
Jimshegs <olawaleogunjimi@gmail.com>
2016-10-31 01:34:56 +0100

    Add files via upload

---
+++ config/blog.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+	'name' => 'COCOAU',
+	'title' => 'Church of Christ Obafemi  Awolowo  University',
+	'subtitle' => 'Our blog',
+	'description' => 'This is our meta description',
+	'author' => 'Jimshegs',
+	'page_image' => 'home-bg.jpg',
+	'post_per_page' => '10',
+	'contact_email' => 'oaucoc@gmail.com',
+	'uploads' => [
+		'storage' => 'local',
+		'webpath' => '/uploads/',
+	],
+];
